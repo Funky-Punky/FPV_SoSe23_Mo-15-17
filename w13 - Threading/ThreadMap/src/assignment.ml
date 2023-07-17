db07422633ca55c62d29fc2fa2b9a9261d987acf
Honas <ge65gom@mytum.de>
2023-07-17 16:50:05 +0200

    week 13 code

--- w13 - Threading/ThreadMap/src/assignment.ml
+++ w13 - Threading/ThreadMap/src/assignment.ml
@@ -5,12 +5,29 @@ type ('k, 'v) request = Put of ('k * 'v) | Get of ('k * 'v option channel)
 
 let start_server () =
   let channel = Event.new_channel () in
-  let rec help assoq_list = failwith "" in
+  let rec help assoq_list =
+    match sync (receive channel) with
+    | Put (key, value) -> help ((key, value) :: assoq_list)
+    | Get (key, ret_chan) ->
+        sync (send ret_chan (List.assoc_opt key assoq_list));
+        help assoq_list
+  in
   let _ = Thread.create help [] in
   channel
 
-let put map key value = failwith ""
-let get map key = failwith ""
+let put map key value = 
+  sync (send map (Put(key, value)))
+
+
+let get_opt map key = 
+  let chan = Event.new_channel () in
+  sync(send map (Get (key, chan)));
+  sync(receive chan)
+
+
+
+
+
 
 (* let create_Map () =
      let channel = Event.new_channel () in
